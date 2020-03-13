e3136805dca8c89ec1ae314b224f23fabd480daf
Rafeal Crema <rafealcrema91@icloud.com>
2020-03-13 17:04:37 -0300

    Finalização do HTML do FA

--- php/db/database.php
+++ php/db/database.php
@@ -7,12 +7,8 @@ $db = new mysqli($servername, $username, $password, $dbname);
 
 if (!$db) {
     echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
- 
-echo "Sucesso ao conectar-se com a base de dados MySQL." . PHP_EOL;
- 
+
 mysqli_close($db);
 ?>
